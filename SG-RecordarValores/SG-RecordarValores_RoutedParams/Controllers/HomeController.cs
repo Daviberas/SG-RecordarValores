c629a5205f45677816cd95a3608bd4ee4a527a52
dbenitez <dbenitez@107-14>
2016-12-16 10:24:56 +0100

    RoutedParams añadidos

---
+++ SG-RecordarValores/SG-RecordarValores_RoutedParams/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SG_RecordarValores_RoutedParams.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            ViewBag.contador = id;
+            return View();
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult IndexPost(int? id)
+        {
+            int contador = 1;
+            if (id != null)
+            {
+                contador = (int) id + 1;
+            }
+
+            return RedirectToAction("Index", new { id = contador });
+        }
+    }
+}
---
+++ SG-RecordarValores/SG-RecordarValores_RoutedParams/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SG_RecordarValores_RoutedParams.MvcApplication" Language="C#" %>
